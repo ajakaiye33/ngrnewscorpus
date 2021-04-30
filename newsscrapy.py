f11f1963f741240636f0b42a7f9147df4915f0bc
Hedgar <ajakzhedgar@gmail.com>
2021-04-30 01:51:21 -0700

    fix bugs

--- newsscrapy.py
+++ newsscrapy.py
@@ -168,14 +168,15 @@ def run(config, limit=2):
             prety_data['newspapers']['saharareporters']['articles'],
             prety_data['newspapers']['thecable']['articles'],
             prety_data['newspapers']['tech_cabal']['articles'],
-            prety_data['newspapers']['tribune']['articles'],
+            prety_data['newspapers']['daily_times']['articles'],
             prety_data['newspapers']['observer']['articles'],
             prety_data['newspapers']['premium_times']['articles'],
             prety_data['newspapers']['hallmark']['articles'],
             prety_data['newspapers']['leadership']['articles'],
-            prety_data['newspapers']['daily_trust']['articles'],
+            prety_data['newspapers']['news_direct']['articles'],
             prety_data['newspapers']['daylight']['articles'],
-            prety_data['newspapers']['pm_news']['articles']]
+            prety_data['newspapers']['pm_news']['articles'],
+            prety_data['newspapers']['biz_watch']['articles']]
 
         for articlez in all_articles:
             for line in articlez:
